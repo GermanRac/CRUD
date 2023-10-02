a284f1791d41ae96ec8e7d8aad3e63d40a26fbfc
German Racines <german.racines@udla.edu.ec>
2023-10-02 18:11:25 -0500

    Implementacion de vista y formato bootstrap
    implementacion en el template gestion indices

--- modulos/factores/models.py
+++ modulos/factores/models.py
@@ -6,4 +6,8 @@ class Indice (models.Model):
     codigo=models.CharField(primary_key=True,max_length=6)
     nombre=models.CharField(max_length=50)
     ponderacion=models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.ponderacion)
  
--- modulos/factores/views.py
+++ modulos/factores/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . models import Indice
 # Create your views here.
+
+def home(request):
+    IndicesListados = Indice.objects.all()
+    return render(request,"gestionIndices.html",{"indices": IndicesListados})
